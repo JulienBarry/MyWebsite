2729b93602e345023393b1a765c5f22445ea2332
JulienBarry <julien-malick.barry@bdk.ch>
2019-03-29 17:00:48 +0100

    auf jeden

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { PreloadAllModules, RouterModule } from '@angular/router';
+
+RouterModule.forRoot(
+  [
+   {
+      path: 'second',
+      loadChildren: './second/second.module#SecondModule'
+   } 
+  ], {preloadingStrategy: PreloadAllModules})
 
 if (environment.production) {
   enableProdMode();
